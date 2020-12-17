2a455221749fc08d660491e7913e399373f74d21
Shen-Jingjing <1337964135@qq.com>
2020-12-17 21:46:35 +0800

    期末大作业

---
+++ Thefinalapplication/app/src/main/java/com/example/thefinalapplication/bean/GoInfo.java
@@ -0,0 +1,61 @@
+package com.example.thefinalapplication.bean;
+
+import com.example.thefinalapplication.R;
+
+import java.util.ArrayList;
+
+public class GoInfo {
+    public int pic_id;
+    public String title;
+    public String desc;
+    public boolean bPressed;
+    public int id;
+    private static int seq = 0;
+
+    public GoInfo(int pic_id, String title, String desc) {
+        this.pic_id = pic_id;
+        this.title = title;
+        this.desc = desc;
+        this.bPressed = false;
+        this.id = this.seq;
+        this.seq++;
+    }
+    private static int[] gridImageArray = {R.drawable.pic_01, R.drawable.pic_02, R.drawable.pic_03
+            , R.drawable.pic_04, R.drawable.pic_05, R.drawable.pic_06, R.drawable.pic_07
+            , R.drawable.pic_08, R.drawable.pic_09, R.drawable.pic_10};
+    private static String[] gridTitleArray = {"登录", "超市", "百货", "便利店",
+            "地摊", "食杂店", "饭店", "餐厅", "会所", "菜市场"};
+
+    public static ArrayList<GoInfo> getDefaultGrid() {
+        ArrayList<GoInfo> gridArray = new ArrayList<GoInfo>();
+        for (int i = 0; i < gridImageArray.length; i++) {
+            gridArray.add(new GoInfo(gridImageArray[i], gridTitleArray[i], null));
+        }
+        return gridArray;
+    }
+
+
+
+    private static int[] stagImageArray = {R.drawable.cloth1, R.drawable.cloth2, R.drawable.cloth3
+            , R.drawable.cloth4, R.drawable.cloth5, R.drawable.cloth6, R.drawable.cloth7
+            , R.drawable.cloth8, R.drawable.cloth9, R.drawable.cloth10, R.drawable.cloth11
+            , R.drawable.cloth12, R.drawable.cloth13, R.drawable.cloth14, R.drawable.cloth15
+            , R.drawable.cloth16, R.drawable.cloth17, R.drawable.cloth18, R.drawable.cloth19
+            , R.drawable.cloth20, R.drawable.cloth21, R.drawable.cloth22, R.drawable.cloth23};
+    private static String[] stagTitleArray = {"促销价", "惊爆价", "跳楼价", "白菜价", "清仓价", "割肉价",
+            "实惠价", "一口价", "满意价", "打折价", "腰斩价", "无人问津", "算了吧", "大声点",
+            "嘘嘘", "嗯嗯", "呼呼", "呵呵", "哈哈", "嘿嘿", "嘻嘻", "嗷嗷", "喔喔"};
+
+    public static ArrayList<GoInfo> getDefaultStag() {
+        ArrayList<GoInfo> stagArray = new ArrayList<GoInfo>();
+        for (int i = 0; i < stagImageArray.length; i++) {
+            stagArray.add(new GoInfo(stagImageArray[i], stagTitleArray[i], null));
+        }
+        return stagArray;
+    }
+
+
+
+
+
+}
