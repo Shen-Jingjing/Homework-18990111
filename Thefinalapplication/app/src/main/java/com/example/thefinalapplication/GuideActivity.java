e189b1b5caf8aa8b53290772bc11f654bad27f3e
Shen-Jingjing <1337964135@qq.com>
2020-12-17 22:11:14 +0800

    期末大作业

--- Thefinalapplication/app/src/main/java/com/example/thefinalapplication/GuideActivity.java
+++ Thefinalapplication/app/src/main/java/com/example/thefinalapplication/GuideActivity.java
@@ -8,7 +8,7 @@ import androidx.viewpager.widget.ViewPager;
 import com.example.thefinalapplication.adapter.LaunchImproveAdapter;
 
 public class GuideActivity extends AppCompatActivity {
-    
+
     // 声明引导页面的图片数组
     private int[] lanuchImageArray = {R.drawable.guide_bg1,
             R.drawable.guide_bg2, R.drawable.guide_bg3, R.drawable.guide_bg4};
